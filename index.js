11603fd2c481ee45bd7a1e1dacfdfa379c6969cc
nishantsainiprofile <nishantkrj25@gmail.com>
2025-03-26 23:15:15 +0530

    Update index.js
    udate the origin

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const app=express();
 
 app.use(
   cors({
-    origin: "https://electronic-based-project.vercel.app", // No trailing slash
+    origin: "*", // No trailing slash
     // origin: 'http://localhost:3002', // No trailing slash
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
