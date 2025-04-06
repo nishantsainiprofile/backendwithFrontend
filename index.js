46a49d98351d4152713c47fef322d1327a95540f
nishantsainiprofile <nishantkrj25@gmail.com>
2025-04-06 21:05:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1840,7 +1840,7 @@ app.post("/api/save-order", async (request, response) => {
   }
 });
 /*----------------API to receive the data from Orderstatus----------------*/
-app.get("/order-status", async (req, res) => {
+app.get("/api/order-status", async (req, res) => {
   try {
     const { email } = req.body;
     if (!email) {
