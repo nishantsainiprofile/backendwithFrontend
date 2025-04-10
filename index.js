428f7141d91ddf11fd79a66331132d2d4f389b51
nishantsainiprofile <nishantkrj25@gmail.com>
2025-04-10 20:12:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -783,8 +783,8 @@ const app=express();
 
 app.use(
   cors({
-    origin: "https://electronic-based-project.vercel.app", // No trailing slash
-    // origin: '*',
+    // origin: "https://electronic-based-project.vercel.app", // No trailing slash
+    origin: '*',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
